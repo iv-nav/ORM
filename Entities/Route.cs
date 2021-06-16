a5ea503d603783cf85b1061978f9c17ddb2cbbf0
Иван Навроцкий <ivnavrotskiy@yandex.ru>
2021-06-16 07:51:42 +0300

    Add project files.

---
+++ Entities/Route.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using multipurposeOrm.Attributes;
+
+namespace ORM
+{
+    class Route
+    {
+        [MyAttr("id")]
+        private long id;
+        public string number;
+        public string name;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace ORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var routeObj = JObject.FromObject(new { id = 3 });
+            var abc = new Route();
+            abc
+            var route = routeObj.ToObject<Route>();            
+            Console.ReadLine();
+        }
+    }
+}
